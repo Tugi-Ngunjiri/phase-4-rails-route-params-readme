4f58256b7ac559e42613b6ba805a245f00e0b190
tugi <blueqwinn@gmail.com>
2023-06-20 08:52:08 +0300

    done with students controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,3 +3,9 @@
       students =  Student.all 
       render json: students
     end
+
+    def show 
+        students = Student.find_by(id:params[:id])
+        render json: students.to_json(only:[:id, :first_name, :last|_name,  :grade])
+    end
+end
